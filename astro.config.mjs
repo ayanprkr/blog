6c4980f8d7b342711b17441831afad9032b51a15
MetalOoze <38457291+MetalOoze05@users.noreply.github.com>
2022-10-16 12:50:14 +0530

    feat: major ui updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import  tailwind from '@astrojs/tailwind';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), tailwind()],
+	markdown: {
+		shikiConfig: {
+			theme: 'one-dark-pro',
+			wrap: true
+		}
+	}
 });
